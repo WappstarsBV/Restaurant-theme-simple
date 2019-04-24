8ee695b8bc31a8a8898f9c70c3801683cb7d256f
PexTzar <ratkovic.petar@gmail.com>
2019-04-24 09:46:55 +0200

    testing

--- header.php
+++ header.php
@@ -49,7 +49,7 @@
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
+				'menu_id'        => 'primary-menu'
 			) );
 			?>
 		</nav><!-- #site-navigation -->
