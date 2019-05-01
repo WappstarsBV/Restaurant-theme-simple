e68b8cb3a566c7d3d76aca502e43fee7fb7d03d4
PexTzar <ratkovic.petar@gmail.com>
2019-05-01 16:16:10 +0200

    Over ons fix

--- template-parts/over-ons/header.php
+++ template-parts/over-ons/header.php
@@ -1,16 +1,22 @@
 <?php
+$imageOne = get_field('afbeelding_1_over_ons', 'option')['url'];
+$imageTwo = get_field('afbeelding_2_over_ons', 'option')['url'];
+
 while ( have_posts() ) :
     the_post(); ?>
 <div class="rt-uno-over-header">
-    <div class="top-curve">
-        <?php include ( get_stylesheet_directory() . '/images/white-curve.svg' ); ?>
-    </div><!-- /.top-curve -->
     <header class="over-header container">
         <div class="over-header-content">
-        <div class="clearfix">
+            <?php if($imageOne) { ?>
+            <div class="top-header-image" style="background-image: url(<?php echo $imageOne; ?>)"></div><!-- /.top-header-image -->
+            <?php } ?>
+
+            <?php if($imageTwo) { ?>
+            <div class="bottom-header-image" style="background-image: url(<?php echo $imageTwo; ?>)"></div><!-- /.top-header-image -->
+            <?php } ?>
+
             <?php the_title( '<h1>', '</h1>' ); ?>
             <p class="over-intro"><?php echo strip_tags(get_the_content()); ?></p><!-- /.over-intro -->
-        </div>
         </div><!-- /.over-header-content -->
     </header><!-- .entry-header -->
 </div><!-- /.rt-uno-over-header -->
